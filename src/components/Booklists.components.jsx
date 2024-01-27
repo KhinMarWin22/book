e03b4ec7050d982df2b19391a35a5c1c438f55c2
KhinMarWin22 <khinmarw658@gamil.com>
2024-01-27 12:37:22 +0630

    edit

--- src/components/Booklists.components.jsx
+++ src/components/Booklists.components.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 const BooklistsComponents = ({data}) => {
   return (
-   <div className='  w-[340px] h-[490px]  rounded-lg '>
+   <div className='  w-[320px] h-[480px]  rounded-lg '>
      <div className="max-w-sm rounded border overflow-hidden shadow-lg">
         <Link to={`/detail/${data.id}`}>
-        <img className=" w-full h-[400px]     hover:opacity-60 hover:scale-105 duration-500     rounded-t-lg   "  src={data.image} alt="Sunset in the mountains" /></Link>
+        <img className=" w-full h-[320px]  aspect-auto    hover:opacity-60 hover:scale-105 duration-500     rounded-t-lg   "  src={data.image} alt="Sunset in the mountains" /></Link>
  
   <div className="px-6 py-4">
     <div className="font-bold text-2xl font-serif mb-2 line-clamp-1">{data.book}</div>
